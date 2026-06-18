a86b4cd466ca90210a3a125c8dd9cd654add368f
srinigaa27 <sriniga319@gmail.com>
2026-06-18 13:37:10 +0530

    Added html code

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,39 +1,40 @@
+// javascript code 
 import{useState,useEffect}from "react";
 function App(){
-  const [name,setName]= useState("");
-  const [email,setEmail]= useState("");
-  const [department,setDepartment]= useState("")
+    const [name,setName]= useState("");
+    const [email,setEmail]= useState("");
+    const [department,setDepartment]= useState("")
     const [age,setAge]= useState("")
     const [students,setStudents]= useState([]);
     const [editIndex,setEditIndex]= useState(null);
 
     useEffect(()=> {
-      const savedStudents =
-      JSON.parse(
+    const savedStudents =
+    JSON.parse(
         localStorage.getItem("students")
-      )|| [];
-      setStudents(savedStudents);
+    )|| [];
+    setStudents(savedStudents);
     } , []);
 
     const handleSubmit = () => {
 
-  if(
+    if(
     name === "" ||
     email === "" ||
-      department === "" ||
-      age === "" 
+    department === "" ||
+    age === "" 
     ) {
 
-        alter("Please fill all fields");
+        alert("Please fill all fields");
 
         return;
 
 }
 const student = {
-          name,
-          email,
-          department,
-          age
+    name,
+    email,
+    department,
+    age
 };
 
 if(editIndex !== null){
@@ -44,35 +45,36 @@ if(editIndex !== null){
     setStudents(updatedStudents);
 
     localStorage.setItem(
-      "students",
-      JSON.stringify(updatedStudents)
+    "students",
+    JSON.stringify(updatedStudents)
     );
         setEditIndex(null);
 }
 else{
     const updatedStudents =[
-      ...students,
-      student
+    ...students,
+    student
     ];
-      setStudents(updatedStudents);
+    setStudents(updatedStudents);
 
-      localStorage.setItem(... 
-      );
+    localStorage.setItem(... 
+        "students"
+    );
 localStorage.setItem(
-  "students",
-  JSON.stringify(updatedStudents)
+    "students",
+    JSON.stringify(updatedStudents)
 );
 }
-      setName("")
+setName("")
 
-      setEmail("")
+    setEmail("")
 
-      setDepartment("")
+    setDepartment("")
 
-      setAge("")
+    setAge("")
 };
 
-      const handleEdit =(index) => {
+    const handleEdit =(index) => {
         const student =students[index];
 
         setName(student.Name);
@@ -84,23 +86,23 @@ localStorage.setItem(
         setAge(student.age);
 
         setEditIndex(index);
-      };
+    };
 
-      const handleDelete = (index) => {
+    const handleDelete = (index) => {
 
         const updatedStudents =
 
-          students.filter(
+        students.filter(
             (student,i) => i !== index
-          );
+        );
         setStudents(updatedStudents);
 
         localStorage.setItem(
-          "students",
-          JSON.stringify(updatedStudents)
+        "students",
+        JSON.stringify(updatedStudents)
         );
-      };
-      const inputStyle = {
+    };
+    const inputStyle = {
         width: "100%",
 
         padding: "10px",
@@ -112,8 +114,8 @@ localStorage.setItem(
         border: "1px solid  #ccc",
 
         boxSizing: "border-box"
-      };
-      const buttonStyle = {
+    };
+    const buttonStyle = {
         width: "100%",
 
         padding: "12px",
@@ -124,170 +126,170 @@ localStorage.setItem(
 
         cursor: "pointer"
 };
-      export default App;
-      //html code in javascript
-  return(
+    export default App;
+    //html code ion javascript
+    return(
     <div
     style={{
-      width: "700px",
-      margin: "30px auto",
-      padding: "25px",
-      borderRadius: "15px",
-      boxShadow:
-      "0 0 15px rgba(0,0,0,0.2)"
+    width: "700px",
+    margin: "30px auto",
+    padding: "25px",
+    borderRadius: "15px",
+    boxShadow:
+    "0 0 15px rgba(0,0,0,0.2)"
     }}
     >
-      <h1
+    <h1
         style={{
-          textAlign:"center"
+        textAlign:"center"
         }}
-      >
-      learner registration 
-      </h1>
-      <input
-      type="text"
+    >
+    learner registration 
+    </h1>
+    <input
+    type="text"
 
-      placeholder="Enter name"
+    placeholder="Enter name"
 
-      value={name}
+    value={name}
 
-      onchange={(e)=>
+    onchange={(e)=>
 
         setName(e.target.value)
-      }
-      style={inputStyle}
-      />
-      <input
-      type="email"
+    }
+    style={inputStyle}
+    />
+    <input
+    type="email"
 
-      placeholder="Enter Email"
+    placeholder="Enter Email"
 
-      value={email}
+    value={email}
 
-      onchange={(e)=>
+    onchange={(e)=>
 
         setEmail(e.target.value)
-      }
-      style={inputStyle}
-      />
-      <input
-      type="text"
+    }
+    style={inputStyle}
+    />
+    <input
+    type="text"
 
-      placeholder="Enter Department"
+    placeholder="Enter Department"
 
-      value={Department}
+    value={Department}
 
-      onchange={(e)=>
+    onchange={(e)=>
 
         setDepartment(e.target.value)
-      }
-      style={inputStyle}
-      />
-      <input
-      type="number"
+    }
+    style={inputStyle}
+    />
+    <input
+    type="number"
 
-      placeholder="Enter Age"
+    placeholder="Enter Age"
 
-      value={age}
+    value={age}
 
-      onchange={(e)=>
+    onchange={(e)=>
 
         setAge(e.target.value)
-      }
-      style={inputStyle}
-      />
-      <button
+    }
+    style={inputStyle}
+    />
+    <button
         onClick={handleSubmit}
         
-          style={buttonStyle}
+        style={buttonStyle}
         >
-          {
+        {
             editIndex !== null
             ?
             "update Student"
             :
             "add student"
-          }
+        }
         </button>
 
         <h2>
-          student list
+        student list
         </h2>
 
         <table
         border="1"
         cellpadding="10"
         style={{
-          width="100%",
+        width:"100%",
 
-          borderCollapse:
+        borderCollapse:
 
-          "collapse"
+        "collapse"
         }}
         >
-          <thead>
+        <thead>
             <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Department</th>
-              <th>Age</th>
-              <th>Actions</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Department</th>
+            <th>Age</th>
+            <th>Actions</th>
             </tr>
-          </thead>
+        </thead>
 
-          <tbody>
+        <tbody>
 
             {
-              students.map(
+            students.map(
 
                 (student,index)=> (
-                  <tr key={index}>
+                <tr key={index}>
 
                     <td>
-                      {student.name}
+                    {student.name}
                     </td>
 
                     <td>
-                      {student.email}
+                    {student.email}
                     </td>
 
                     <td>
-                      {student.department}
+                    {student.department}
                     </td>
 
                     <td>
-                      {student.age}
+                    {student.age}
                     </td>
 
                     <td>
-                      <button
-                      onClick={()  => 
-                      handleEdit(index)
-                      }
-                      >
-                      Edit
-                      </button>
-                      <button
-                      onClick={() =>
+                    <button
+                    onClick={()  => 
+                    handleEdit(index)
+                    }
+                    >
+                    Edit
+                    </button>
+                    <button
+                    onClick={() =>
                         handleDelete(index)
-                      }
-                      
-                      style={{
+                    }
+                    
+                    style={{
                         marginLeft:
                         "10px"
-                      }}
-                      >
+                    }}
+                    >
                         Delete
-                      </button>
+                    </button>
                     </td>
-                  </tr>
+                </tr>
                 )
-              )
+        )
             }
-          </tbody>
+    </tbody>
 
         </table>
 
     </div>
-  );
+);
 }
